87a642f80b4c1619268bf895e3f9dcb6a0211b40
a.bimael2000@hotmail.com <a.bimael2000@hotmail.com>
2024-12-13 16:42:49 -0300

    att

--- app/Http/Controllers/EcommController.php
+++ app/Http/Controllers/EcommController.php
@@ -397,6 +397,7 @@ class EcommController extends Controller
 
         $responseData = $this->payment($request, $order_cart);
 
+        return response()->json($responseData);
         if (isset($responseData)) {
             $payment = $this->registerOrder($request, $responseData);
             return redirect()->awpay($payment['url']);
